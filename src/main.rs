ce708594e08ad23aab71fe6e04299c3d1d22f51d
Robert Zieba <robert@zieba.site>
2023-02-08 21:21:49 -0700

    Initial binary image loading support

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use bibe_emu::state::State;
-use bibe_instr::{
-	Instruction,
-	Register,
-	BinOp,
-	rrr,
-	rri,
+use bibe_emu::{
+	memory::image::Image,
+	state::State,
 };
-
+use bibe_instr;
+use bibe_asm_proc::asm;
+use clap::{ Arg, Command };
 use std::fs::File;
 use simplelog::{
 	Config as LogConfig,
@@ -16,50 +14,17 @@ use simplelog::{
 
 fn main() {
 	let log_file = File::create("log.txt").expect("Failed to create log file");
-	let mut s = State::new();
+	let matches = Command::new("as")
+		.arg(Arg::new("input").required(true))
+		.get_matches();
 
 	WriteLogger::init(LevelFilter::Debug, LogConfig::default(), log_file).expect("Failed to init logger");
 	
-	s.execute(&[
-		Instruction::Rri(rri::Instruction {
-			op: BinOp::Add,
-			cond: rri::Condition::Al,
-			dest: Register::r1(),
-			src: Register::r0(),
-			imm: 2
-		}),
-		Instruction::Rri(rri::Instruction {
-			op: BinOp::Add,
-			cond: rri::Condition::Al,
-			dest: Register::r2(),
-			src: Register::r0(),
-			imm: 3
-		}),
-		Instruction::Rrr(rrr::Instruction {
-			op: BinOp::Add,
-			dest: Register::r3(),
-			lhs: Register::r1(),
-			rhs: Register::r2(),
-			shift: rrr::Shift {
-				kind: rrr::ShiftKind::Shl,
-				shift: 0
-			}
-		}),
-		Instruction::Rri(rri::Instruction {
-			op: BinOp::Cmp,
-			cond: rri::Condition::Al,
-			dest: Register::r0(),
-			src: Register::r3(),
-			imm: 5,
-		}),
-		Instruction::Rri(rri::Instruction {
-			op: BinOp::Add,
-			cond: rri::Condition::Eq,
-			dest: Register::r4(),
-			src: Register::r0(),
-			imm: 1,
-		}),
-	]);
+	let mut image = File::open(&matches.get_one::<String>("input").unwrap()).expect("Failed to open binary");
+	let mut s = State::new(Box::new(Image::load(&mut image)));
 
-	println!("{:?}", s);
+	loop {
+		s.execute();
+		println!("{:?}", s);
+	}
 }
